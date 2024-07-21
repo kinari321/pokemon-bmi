7ee48daabdc072da2e62aca1501d7ac20f7135ae
kinari321 <abcdream321@yahoo.co.jp>
2024-07-21 17:49:18 +0900

    pokemonをリスト化して表示する

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
   import { ref, onMounted } from 'vue';
 
+  let id = 0;
   const pokemonName = ref('');
   const pokemon = ref(null);
+  const pokemons = ref([]);
 
   async function fetchPokemon() {
     try {
@@ -13,11 +15,15 @@
         throwError(res);
       }
       pokemon.value = await res.json();
+      console.log(pokemon.value);
+      pokemons.value.push({ id: id++, data: pokemon.value });
+      console.log(pokemons);
     } catch (err) {
       console.error('Failed to fetch Pokemon data:', err);
     }
 
-    pokemonName.value = '';
+    // pokemon.value = null;
+    // pokemonName.value = '';
   }
 
   function throwError(err) {
@@ -40,24 +46,32 @@
   </div>
   <input v-model="pokemonName" placeholder="Type here" />
   <button @click="fetchPokemon">ポケモンを検索</button>
-  <p v-if="!pokemon"></p>
-  <div v-else>
-    <p>名前 : {{ pokemon?.name }}</p>
-    <p>高さ : {{ pokemon?.height / 10 }} m</p>
-    <p>重さ : {{ pokemon?.weight / 10 }} kg</p>
-    <p>
-      BMI値 :{{ (pokemon?.weight / (pokemon?.height * pokemon?.height)) * 10 }}
-    </p>
-    <p>
-      ポケモン番号 : No.{{ pokemon?.id }}
-      （ポケモン図鑑は
-      <a
-        :href="`https://zukan.pokemon.co.jp/detail/${pokemon?.id}`"
-        target="_blank"
-        rel="noopener noreferrer"
-        >こちら</a
-      >）
-    </p>
-    <pre>{{ pokemon }}</pre>
-  </div>
+  <ul>
+    <li v-for="pokemon in pokemons" :key="pokemon.id">
+      <p v-if="!pokemon"></p>
+      <div v-else>
+        <p>名前 : {{ pokemon?.data.name }}</p>
+        <p>高さ : {{ pokemon?.data.height / 10 }} m</p>
+        <p>重さ : {{ pokemon?.data.weight / 10 }} kg</p>
+        <p>
+          BMI値 :{{
+            (pokemon?.data.weight /
+              (pokemon?.data.height * pokemon?.data.height)) *
+            10
+          }}
+        </p>
+        <p>
+          ポケモン番号 : No.{{ pokemon?.data.id }}
+          （ポケモン図鑑は
+          <a
+            :href="`https://zukan.pokemon.co.jp/detail/${pokemon?.data.id}`"
+            target="_blank"
+            rel="noopener noreferrer"
+            >こちら</a
+          >）
+        </p>
+        <!-- <pre>{{ pokemon }}</pre> -->
+      </div>
+    </li>
+  </ul>
 </template>
