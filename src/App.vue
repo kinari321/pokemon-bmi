5509a827fa2d3ee7ae14fe6ddbb5b49319849489
kinari321 <abcdream321@yahoo.co.jp>
2024-07-21 16:42:50 +0900

    取得したjsonデータから必要な項目を抽出

--- src/App.vue
+++ src/App.vue
@@ -29,18 +29,34 @@
   <div>
     <p>ポケモンの英語名または番号を入力してください。</p>
     <p>
-      ポケモンの番号については<a
-        href="https://wiki.xn--rckteqa2e.com/wiki/%E3%83%9D%E3%82%B1%E3%83%A2%E3%83%B3%E4%B8%80%E8%A6%A7"
-        target="_blank"
-        rel="noopener noreferrer"
-        >こちら</a
-      >を参照
+      <div>ポケモン番号については
+        <a
+          href="https://wiki.xn--rckteqa2e.com/wiki/%E3%83%9D%E3%82%B1%E3%83%A2%E3%83%B3%E4%B8%80%E8%A6%A7"
+          target="_blank"
+          rel="noopener noreferrer"
+          >こちら</a
+        >を参照
+      </div>
     </p>
   </div>
   <input v-model="pokemonName" placeholder="Type here" />
   <p>{{ pokemonName }}</p>
   <button @click="fetchPokemonData">ポケモンを検索</button>
-  <!-- <p v-if="!pokemonData">{{ loadingText }}</p> -->
-  <!-- <pre v-else>{{ pokemonData }}</pre> -->
-  <pre>{{ pokemonData }}</pre>
+  <p v-if=!pokemonData></p>
+  <p v-else>
+    <p>名前 : {{ pokemonData?.name }}</p>
+    <p>高さ : {{ pokemonData?.height / 10}} m</p>
+    <p>重さ : {{ pokemonData?.weight / 10 }} kg</p>
+    <p>BMI値 :{{ pokemonData?.weight / (pokemonData?.height * pokemonData?.height) * 10}}</p>
+    <p>
+      ポケモン番号 : No.{{ pokemonData?.id }}
+      （ポケモン図鑑は
+          <a
+          :href="`https://zukan.pokemon.co.jp/detail/${pokemonData?.id}`"
+            target="_blank"
+            rel="noopener noreferrer"
+        >こちら</a>）
+    </p>
+    <pre>{{ pokemonData }}</pre>
+  </p>
 </template>
